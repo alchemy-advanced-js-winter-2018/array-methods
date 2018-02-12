a291fc8b34b70aad6924ea8442804e15e9676abe
CharlyWelch <blwbiology@gmail.com>
2018-02-12 13:30:45 -0800

    set up environment, create empty modules

---
+++ test/array-methods.test.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+const forEach = require('../lib/for-each');
+
+describe('array methods', () => {
+
+    // function testindexes(){};
+
+    it('forEach', () => {
+        const array = [1, 2, 3];
+
+        let result = '';
+
+        forEach(array, i => {
+            result += i;
+        });
+    });
+});
