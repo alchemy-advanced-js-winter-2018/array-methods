9cc5c1964818a1452eaa05cde9b3c840f770f56f
CharlyWelch <blwbiology@gmail.com>
2018-02-12 16:35:37 -0800

    add whitespace to tests

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -51,75 +51,93 @@ describe('array methods', () => {
 
     it('findIndex', () => {
         const array = [1, 2, 3, 4, 5];
+
         const result = findIndex(array, (item, index) => {
+            
             if(item === 5) {
                 return true;
             }
         });
+
         assert.equal(result, 4);
     });
 
 
     it('findIndex with item not present', () => {
         const array = [1, 2, 3, 4, 5];
+
         const result = findIndex(array, (item, index) => {
+            
             if(item === 6) {
                 return true;
             }
         });
+
         assert.equal(result, -1);
     });
 
 
     it('every', () => {
         const array = [1, 2, 3, 4, 5];
+
         const result = every(array, (item, index) => {
+           
             if(item > 0) {
                 return true;
             } else {
                 return false;
             }
         });
+
         assert.equal(result, true);
     });
 
 
     it('every for false', () => {
         const array = [1, 2, 3, 4, 5];
+        
         const result = every(array, (item, index) => {
+            
             if(item < 0) {
                 return true;
             } else {
                 return false;
             }
         });
+
         assert.equal(result, false);
     });
 
 
     it('reduce', () => {
         const array = [1, 2, 3, 4, 5];
+        
         const result = reduce(array, (sum, num, i)  => {
             return sum + num;
         });
+        
         assert.equal(result, 15);
     });
 
 
     it('reduce for multiplication', () => {
         const array = [1, 2, 3, 4, 5];
+        
         const result = reduce(array, (acc, num, i)  => {
             return acc * num;
         });
+        
         assert.equal(result, 120);
     });
 
 
     it('reduce with initializer', () => {
         const array = [1, 2, 3, 4, 5];
+        
         const result = reduce(array, (sum, num, i)  => {
             return sum + num;
         }, 5);
+        
         assert.equal(result, 20);
     });
 });
